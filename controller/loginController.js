877decd24eaf851c4f534492c002c4b7a6d74eaa
Fatin <fatin.qistina998@gmail.com>
2020-08-03 10:06:29 +0800

    Initial Commit

---
+++ controller/loginController.js
@@ -0,0 +1,170 @@
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/mydb';
+var connectionString = 'mongodb://localhost:27017/mydb';
+
+module.exports = (function(app){
+  app.get('/', function(req,res){
+    res.render('login');
+  });
+  app.get('/register',function(req,res){
+    res.render('register');
+  });
+
+app.get('/error', function(req,res){
+    res.render('error');
+  });
+
+app.get('/pengurusanTugas', function(req,res){
+MongoClient.connect(url, function(err, db) {
+      const cursor=db.collection('tasksMgmt').find().toArray()
+      .then(results=>{
+     res.render('pengurusanTugas.ejs', { tasksMgmt: results })
+      }).catch(error=>console.error(error));
+      });
+  });
+
+
+app.get('/completeprofile', function(req,res){
+    // res.render('completeprofile');
+     MongoClient.connect(url, function(err, db) {
+    const cursor=db.collection('userprofile').find().toArray()
+    .then(results=>{
+     res.render('completeprofile.ejs', { userprofile: results })
+      }).catch(error=>console.error(error));
+  });
+   });
+
+
+app.get('/manageMeetingInvitation', function(req,res){
+     MongoClient.connect(url, function(err, db) {
+    const cursor=db.collection('userprofile').find().toArray()
+    .then(results=>{
+     res.render('manageMeetingInvitation.ejs', { userprofile: results })
+      }).catch(error=>console.error(error));
+  });
+   });
+
+app.get('/viewMeetingInfo', function(req,res){
+     MongoClient.connect(url, function(err, db) {
+    const cursor=db.collection('meetingInfo').find().toArray()
+    .then(results=>{
+     res.render('viewMeetingInfo.ejs', { meetingInfo: results })
+      }).catch(error=>console.error(error));
+  });
+   });
+
+app.get('/meetingInfo', function(req,res){
+       MongoClient.connect(url, function(err, db) {
+    const cursor=db.collection('tasksMgmt').find().toArray()
+    .then(results=>{
+     res.render('meetingInfo.ejs', { tasksMgmt: results })
+      }).catch(error=>console.error(error));
+  });
+   });
+
+// Login TO DB==================================================================
+  app.post('/demo',urlencodedParser,function(req,res){
+   MongoClient.connect(url, function(err, db) {
+   db.collection('userprofile').findOne({ name: req.body.name}, function(err, user) {
+             if(user ===null){
+              console.log("Empty");
+               res.render('error');
+            }else if (user.name === req.body.name && user.pass === req.body.pass){
+            const cursor=db.collection('userprofile').find().toArray()
+    .then(results=>{
+     res.render('completeprofile.ejs', { userprofile: results })
+      }).catch(error=>console.error(error))
+          } else {
+            console.log("Credentials wrong");
+            res.render('error');
+          }
+   });
+ });
+});
+
+//save tasks to MongoDB================================================================
+  app.post('/pengurusanTugas',urlencodedParser,function(req,res){
+   var obj = JSON.stringify(req.body);
+   console.log("Final reg Data : "+obj);
+   var jsonObj = JSON.parse(obj);
+      MongoClient.connect(url, function(err, db) {
+      db.collection("tasksMgmt").insertOne(jsonObj, function(err, res) {
+     if (err) throw err;
+     console.log("1 document inserted");
+     db.close();
+      });
+      const cursor=db.collection('tasksMgmt').find().toArray()
+      .then(results=>{
+     res.render('pengurusanTugas.ejs', { tasksMgmt: results })
+      }).catch(error=>console.error(error));
+      });
+    });
+
+ app.post('/viewMeetingInfo',urlencodedParser,function(req,res){
+   var obj = JSON.stringify(req.body);
+   console.log("Final reg Data : "+obj);
+   var jsonObj = JSON.parse(obj);
+      MongoClient.connect(url, function(err, db) {
+      db.collection("meetingInfo").insertOne(jsonObj, function(err, res) {
+     if (err) throw err;
+     console.log("1 document inserted");
+     db.close();
+      });
+      const cursor=db.collection('meetingInfo').find().toArray()
+      .then(results=>{
+     res.render('viewMeetingInfo.ejs', { meetingInfo: results})
+      }).catch(error=>console.error(error));
+      });
+    });  
+
+MongoClient.connect(connectionString,{
+  useUnifiedTopology: true
+  }).then(client => {
+ 
+  console.log('Connected to Database')
+  const db = client.db('mydb')
+  //const profile= db.collection('userprofile')
+
+  app.get('/getdetails',(req,res)=>{
+    const cursor=db.collection('userprofile').find().toArray()
+    .then(results=>{
+     res.render('completeprofile.ejs', { userprofile: results })
+      }).catch(error=>console.error(error))
+  })
+  app.get('/getInvitation',(req,res)=>{
+    const cursor=db.collection('userprofile').find().toArray()
+    .then(results=>{
+     res.render('manageMeetingInvitation.ejs', { userprofile: results })
+      }).catch(error=>console.error(error))
+  })
+  app.get('/getMeetingInfo',(req,res)=>{
+    const cursor=db.collection('tasksMgmt').find().toArray()
+    .then(results=>{
+     res.render('meetingInfo.ejs', { tasksMgmt: results })
+      }).catch(error=>console.error(error))
+  })
+  app.get('/getUserDetail',(req,res)=>{
+    const cursor=db.collection('userprofile').find().toArray()
+    .then(results=>{
+     res.render('meetingInfo.ejs', { userprofile: results })
+      }).catch(error=>console.error(error))
+  })
+  app.get('/pengurusanTugas',(req,res)=>{
+    const cursor=db.collection('tasksMgmt').find().toArray()
+    .then(results=>{
+     res.render('pengurusanTugas.ejs', { tasksMgmt: results })
+      }).catch(error=>console.error(error))
+  })
+   app.delete('/pengurusanTugas', (req, res) => {
+      db.collection('tasksMgmt').deleteOne()
+        .then(result => {
+          console.log('Task deleted')
+          res.json('Task deleted')
+          res.render('pengurusanTugas.ejs', { tasksMgmt: results })
+        })
+        .catch(error => console.error(error))
+    })
+})
+})
